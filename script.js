fc402b1c15f239102cdc5818dba263bc89f5b043
julia cereda <julia.cereda@aluno.senai.br>
2026-04-10 08:56:33 -0300

    ADD: criando as funções

--- script.js
+++ script.js
@@ -6,8 +6,37 @@ const body = document.querySelectorAll('.body');
 const newsArticle = document.querySelectorAll('.news'); 
 
 const loadPreferences = ()  =>{
+    //cor
+    const savedColor = localStorage.getItem('primaryColor');
+    if (savedColor) {
+        document.documentElement.style.setProperty('--primary-color', savedColor);
+        colorPicker.value = savedColor;
+    }
 
+    //cor dos artigos
+    const savedArticleColor = localStorage.getItem('articleColor');
+    if (savedArticleColor){
+        document.documentElement.style.setProperty('', savedArticleColor);
+        artcicleColorPicker.value = savedArticleColor;
+
+    }
+
+    //fonte
+    const savedFont = localStorage.getItem('fontFamily');
+    if (savedFont){
+        document.documentElement.style.setProperty('--font-family', savedFont);
+        fontSelect.value = savedFont;
+
+    }
+
+    //tema
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme == 'dark') {
+        body.classList.add('dark');
+        themeToggle.innerHTML = '<span class="material-icons">light_mode</span>';
+    }
 };
+
 
 colorPicker.addEventListener('input', (e) =>{
     const color = e.target.value;
@@ -25,10 +54,24 @@ artcicleColorPicker.addEventListener('input', (e) =>{
 });
 
 fontSelect.addEventListener('change', (e) =>{
+    const fonte = e.target.value;
+    document.documentElement.style.setProperty('--font-family', fonte);
+
+    localStorage.setItem('fonteFamily', fonte);
 
 });
 
 themeToggle.addEventListener('click', () =>{
+    body.classList.toggle('dark');
+    const isDark = body.classList.constains('dark');
+
+    themeToggle.innerHTML = isDark
+    ?'<span class="material-icons">light_mode</span>' //? é o if ternario
+    :'<span class="material-icons">dark_mode</span>';
+
+    //se tem dark voce mantem dark, se nao tiver dark voce muda para o light
+
+    localStorage.setItem('theme', isDark ? 'dark' : 'light');
 
 });
 
